3c7fb03c53747c514f38d9badcb585a9466db10a
Vipin Kumar N <binduvipinkumar39@gmail.com>
2022-03-02 10:57:24 +0530

    Removed all BFS Errors

--- BFS.c
+++ BFS.c
@@ -4,8 +4,7 @@ int a[20][20],q[20],visited[20],n,i,j,f=0,r=-1;
 void bfs(int v)
 {
     for(i=1;i<=n;i++)
-    {
-        if(a[v][i]&&!visited[i])
+       if(a[v][i]&&!visited[i])
         q[++r]=i;
         if(f<=r)
         {
@@ -13,7 +12,6 @@ void bfs(int v)
             printf("%d\t",q[f]);
             bfs(q[f++]);
         }
-    }
 }
 void main()
 {
