9b34870ac76738b43035d17f8894d107aabb5323
Vipin Kumar N <binduvipinkumar39@gmail.com>
2022-03-07 00:00:13 +0530

    modified Addition.c

--- Addition.c
+++ Addition.c
@@ -4,6 +4,6 @@ void main()
     int num1, num2, sum;
     printf("Enter two Numbers : \n");
     scanf("%d%d",&num1,&num2);
-    sum=num1+num1;
+    sum=num1+num2;
     printf("\n Sum = %d",sum);
 }
